1e3c65b5a0f21e1e68a56766d0471cd3d33ebb3f
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2022-01-25 11:44:58 -0800

    Update Nix files

--- Readme.md
+++ Readme.md
@@ -5,3 +5,7 @@ to home-manager in the future(not sure if it's worth it). All the programs that
 I used are installed under the system level because, well, I'm the only one
 using my computer. 
 
+
+I'm experimenting with different keyboard layouts. The halmak layout is pretty
+recent so it didn't get added to Linux's xkbmap yet. Manually adding it in my
+configuration(and copied over the code from here: https://github.com/MadRabbit/halmak )
--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
 { config, lib, pkgs, ... }:
 with lib; {
   imports = [ # Include the results of the hardware scan.
+    <nixos-hardware/lenovo/thinkpad/t480>
     ./hardware-configuration.nix
   ];
 
@@ -13,25 +14,35 @@ with lib; {
   boot.loader.efi.canTouchEfiVariables = true;
   services.upower.enable = true;
   # services.tlp.enable = true;
-  services.tlp = {
-      enable = true;
-    };
+  services.tlp = { enable = true; };
+  powerManagement.powertop.enable = true;
+  nix.autoOptimiseStore = true;
 
   services.udev.extraRules = lib.mkMerge [
     # autosuspend USB devices
-    ''ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"''
+    ''
+      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"''
     # autosuspend PCI devices
-    ''ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"''
+    ''
+      ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"''
     # disable Ethernet Wake-on-LAN
-    ''ACTION=="add", SUBSYSTEM=="net", NAME=="enp*", RUN+="${pkgs.ethtool}/sbin/ethtool -s $name wol d"''
+    ''
+      ACTION=="add", SUBSYSTEM=="net", NAME=="enp*", RUN+="${pkgs.ethtool}/sbin/ethtool -s $name wol d"''
     # KMonad user access to /dev/uinput
-    ''KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput" ''
+    ''
+      KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput" ''
+    ''
+      ACTION=="add|change", KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess", RUN{builtin}+="uaccess"''
+    ''
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", TAG+="uaccess"''
+
   ];
 
   services.auto-cpufreq.enable = true; # power saving
   networking.hostName = "thanawat"; # Define your hostname.
   networking.networkmanager.enable =
     true; # Enables wireless support via wpa_supplicant.
+  networking.enableIPv6 = false;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -71,109 +82,107 @@ with lib; {
 
     # Create an alias for the unstable channel
 
-    # packageOverrides = pkgs: {
-    # unstable = import <nixos-unstable> { # pass the nixpkgs config to the unstable alias # to ensure `allowUnfree = true;` is propagated:
-    # config = config.nixpkgs.config;
-    # };
-    # };
+    packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [ libxkbcommon mesa wayland zlib libpng ];
+      };
+    };
   };
   nixpkgs.overlays = [
-    (import (builtins.fetchTarball
-      "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"))
+    #(import (builtins.fetchTarball
+    #  "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"))
 
     (self: super: {
       steam-run = (super.steam.override {
-        extraLibraries = pkgs: with pkgs; [ libxkbcommon mesa wayland zlib ];
+        extraLibraries = pkgs:
+          with pkgs; [
+            libxkbcommon
+            mesa
+            wayland
+            zlib
+            libpng
+          ];
       }).run;
       mmorph = self.callHackage "mmorph" "1.1.3" { };
     })
   ];
-  nix.binaryCaches =
-    [ "https://cache.nixos.org/" "https://nixcache.reflex-frp.org" ];
-  nix.binaryCachePublicKeys =
-    [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
-
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://nixcache.reflex-frp.org"
+      "https://hydra.iohk.io"
+    ];
+    binaryCachePublicKeys = [
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
   # arbtt - time tracking
   services.arbtt.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    zip
-    unzip
-    neovim
-    mu
-    isync
-    nixfmt
-    brave
-    nyxt
-    firefox
-    alacritty
-    # Emacs and related programs
-    emacsUnstable
-    fish
-    git
-    syncthing
-    syncthing-cli
-    sqlite
-    wordnet
-    ripgrep
-    fd
-    aspell
-    aspellDicts.en
-    aspellDicts.en-computers
-    gnutls
+  environment.systemPackages = with pkgs;
+    [
+      wget
+      zip
+      unzip
+      neovim
+      mu
+      isync
+      brave
+      nyxt
+      firefox
+      alacritty
+      fish
+      git
+      wordnet
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      gnutls
 
-    direnv
-    xmobar
-    pass
-    gnupg
-    mpv
-    htop
-    powertop
-    pulsemixer
-    brightnessctl
-    dunst
-    libnotify
-    lutris
-    gnome3.adwaita-icon-theme
-    cmake
-    haskellPackages.arbtt
-    spotify
-    #diskonaut
-    steam-run
-    # Latex and Minted
-    (texlive.combine {
-      # Example of additional packages, probably unnecessary
-      inherit (texlive) scheme-full minted;
-    })
-    python38Packages.pygments
-    caffeine-ng
+      direnv
+      xmobar
+      pass
+      gnupg
+      mpv
+      htop
+      powertop
+      pulsemixer
+      pavucontrol
+      brightnessctl
+      dunst
+      libnotify
+      lutris
+      spotify
+      #diskonaut
+      steam-run
+      # Latex and Minted
+      (texlive.combine {
+        # Example of additional packages, probably unnecessary
+        inherit (texlive) scheme-full minted;
+      })
+      python38Packages.pygments
 
-    playerctl
-    #shortwave
+      gnuplot
 
-    gnuplot
+      hardinfo
+      hwinfo
+      lshw
+      xboxdrv
 
-    hardinfo
-    hwinfo
-    lshw
-
-    trayer
-    linuxPackages.acpi_call
-
-    graphviz
-    tldr
-
-    # For typescript language server
-    nodePackages.typescript-language-server
-    nodePackages.typescript
-
-    freetube
-    anki
-    lm_sensors
-    (import ./packages/kmonad.nix)
-  ] ++ (import ./programs/programming.nix  pkgs);
+      trayer
+      linuxPackages.acpi_call
+      lm_sensors
+      man-pages
+      man-pages-posix
+      # xorg.xinit
+      (import ./packages/kmonad.nix)
+    ] ++ (import ./programs/programming.nix pkgs);
   programs.steam.enable = true;
   programs.gnupg.agent = {
     enable = true;
@@ -182,35 +191,29 @@ with lib; {
   # Enable https://github.com/target/lorri for easier nix-shell integration
   services.lorri.enable = true;
   services.fwupd.enable = true;
+  services.autorandr.enable = true;
   services.clight = {
     enable = true;
-    settings = {
-      keyboard = {
-        disabled = true;
-      };
-    };
+    settings = { keyboard = { disabled = true; }; };
   };
- location.provider = "geoclue2";
-  services.picom = {
-    enable = true;
-    fade = true;
-    inactiveOpacity = 0.9;
-    shadow = false;
-    fadeDelta = 4;
-    vSync = true;
-    backend = "glx";
-  };
+  location.provider = "geoclue2";
+  # services.picom = {
+  #   enable = true;
+  #   fade = true;
+  #   inactiveOpacity = 0.9;
+  #   shadow = false;
+  #   fadeDelta = 4;
+  #   vSync = true;
+  #   backend = "glx";
+  # };
 
   ## VIRTUALIZATION ##
   ## Virtual Box
-  # virtualisation.virtualbox.host.enable = true; I don't need this right now!
+  # virtualisation.virtualbox.host.enable = true; #I don't need this right now!
   # virtualisation.virtualbox.host.enableExtensionPack = true;
   ## Libvirtd - QEMU
-  virtualisation.libvirtd.enable = true;
-  # users.extraGroups.vboxusers.members = [ "thanawat" ];
+  # virtualisation.libvirtd.enable = true;
 
-  # flathub(for flatpacks)
-  #services.flatpak.enable = true;
   xdg.portal.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -244,19 +247,45 @@ with lib; {
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
   # Enable sound.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
-  };
 
+  # hardware.pulseaudio = {
+  #   enable = true;
+  #   package = pkgs.pulseaudioFull;
+  #  };
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us,th";
+  # services.xserver.xkbVariant = "norman";
   services.xserver.xkbOptions = "grp:ctrls_toggle,ctrl:nocaps";
-
+  services.xserver.extraLayouts = {
+    halmak = {
+      description = "The halmak layout";
+      languages = [ "eng" ];
+      symbolsFile = ./keyboard-layouts/halmak;
+    };
+    engram = {
+      description = "The Engram layout";
+      languages = [ "eng" ];
+      symbolsFile = ./keyboard-layouts/engram;
+    };
+  };
 
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
@@ -265,8 +294,15 @@ with lib; {
   services.xserver.displayManager.autoLogin.user = "thanawat";
   services.xserver.displayManager.defaultSession = "none+xmonad";
   services.xserver.displayManager.lightdm.enable = true;
+# services.xserver.displayManager.startx.enable = true;
   services.xserver.windowManager.xmonad.enable = true;
   services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+  services.xserver.digimend.enable = true;
+  programs.xss-lock = {
+    enable = true;
+    lockerCommand = "${pkgs.i3lock}/bin/i3lock";
+  };
+  environment.shells = with pkgs; [ bashInteractive fish ];
 
   services.syncthing = {
     enable = true;
@@ -274,6 +310,8 @@ with lib; {
     dataDir = "/home/thanawat";
     configDir = "/home/thanawat/.config/syncthing";
   };
+
+  # services.fprintd.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.thanawat = {
     isNormalUser = true;
@@ -286,6 +324,8 @@ with lib; {
       "scannner"
       "input"
       "uinput"
+      "jackaudio"
+      # "libvirtd"
     ]; # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
   };
